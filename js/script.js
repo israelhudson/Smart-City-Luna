9462d125b6e0ec5d6be5a0343cb1cc4385b285d9
Israel <israelhudson@gmail.com>
2017-10-25 17:06:26 -0300

    atualização de erros
    .

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/*$(document).ready(function(){
+	setBindings();
+
+});
+
+function setBindings(){
+	$("nav a").click(function(e)
+	{
+		e.preventDefalut();
+		var sectionID = e.currentTarget.id + "Section";
+
+		$("html body").animate({
+
+			scrollTop: $("a" + sectionID).offset().top
+		},1000)
+	})
+}
+*/
+
